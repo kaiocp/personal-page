52e625adf40f84ad0615033a218a1cf1cdf9bdee
kaiocp <kaiocp7@gmail.com>
2022-05-19 16:01:22 -0300

    feat: hamburguer menu closed as nav link is clicked

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,14 @@
 const btnMobile = document.getElementById('btn');
+const headerLinks = document.querySelectorAll('.header__menu-link');
 
 btnMobile.addEventListener('click', () => {
     const nav = document.querySelector('.header__menu');
     nav.classList.toggle('active');
+});
+
+headerLinks.forEach(el => {
+    el.addEventListener('click', () => {
+        const nav = document.querySelector('.header__menu');
+        nav.classList.remove('active');
+    });
 });
