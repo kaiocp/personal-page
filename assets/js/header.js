c166b811c495e8762699a8cd069da194e6731a33
kaiocp <kaiocp7@gmail.com>
2022-05-21 15:48:58 -0300

    feat: navbar section highlight

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,6 @@
 const btnMobile = document.getElementById('btn');
 const headerLinks = document.querySelectorAll('.header__menu-link');
+const sections = document.querySelectorAll('.section');
 
 btnMobile.addEventListener('click', () => {
     const nav = document.querySelector('.header__menu');
@@ -12,3 +13,18 @@ headerLinks.forEach(el => {
         nav.classList.remove('active');
     });
 });
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.pageYOffset;
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 200;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+            document.querySelector('li a[href*="' + sectionId + '"]').classList.add('highlight');
+        } else {
+            document.querySelector('li a[href*="' + sectionId + '"]').classList.remove('highlight');
+        }
+    })
+})
