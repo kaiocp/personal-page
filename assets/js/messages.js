c326f898d93992cd80f667abe461563ae1e8549c
kaiocp <kaiocp7@gmail.com>
2022-05-28 15:50:30 -0300

    feat: messages page

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.contact__email-form');
+const messagesBox = document.getElementById('messages')
 
 let clientMessages = JSON.parse(localStorage.getItem("client_messages")) || [];
 
@@ -10,21 +11,39 @@ function clearFields() {
     })
 }
 
-form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
+function makeMessagesBoxHTML() {
+    clientMessages.forEach((client) => {
+        messagesBox.innerHTML += `
+        <div class="messages__box">
+            <p><span class="bold">Nome: </span>${client.name}</p>
+            <p><span class="bold">E-mail: </span>${client.email}</p>
+            <p class=messages__box-message><span class="bold">Mensagem: </span>${client.message}</p>
+        </div>
+        `
+    })
+}
 
-    let name = ev.target.elements['name'].value;
-    let email = ev.target.elements['email'].value;
-    let message = ev.target.elements['message'].value;
+if (form) {
+    form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
 
-    let client_message = {
-        'name': name,
-        'email': email,
-        'message': message
-    }
+        let name = ev.target.elements['name'].value;
+        let email = ev.target.elements['email'].value;
+        let message = ev.target.elements['message'].value;
 
-    clientMessages.push(client_message);
-    localStorage.setItem('client_messages', JSON.stringify(clientMessages));
+        let client_message = {
+            'name': name,
+            'email': email,
+            'message': message
+        }
 
-    clearFields();
-});
+        clientMessages.push(client_message);
+        localStorage.setItem('client_messages', JSON.stringify(clientMessages));
+
+        clearFields();
+    });
+}
+
+if (clientMessages.length >= 1) {
+    makeMessagesBoxHTML();
+}
